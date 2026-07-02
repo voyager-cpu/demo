f9689208fe2d54201aa3459c0e47a5ab6414ad7f
voyager-cpu <84560951+voyager-cpu@users.noreply.github.com>
2026-07-02 17:16:13 +0800

    Update self.tsx

--- self.tsx
+++ self.tsx
@@ -3,7 +3,7 @@ Thank you for giving me the opportunity of this interview. My name is Qian Wenme
 
 Since graduation, I have been engaged in Java development. The technology stacks I use in my work include Spring, SpringCloud, Springboot, Mybatis, Mysql, Redis, and other commonly used technology stacks.
 
-Since I started working, I have been engaged in Java development, and I have used technology stacks in my work, including Spring,SpringCloud, Springboot, Mybatis, Mysql, Paraphrase and other common technology stacks.
+Since I started working, I have been engaged in Java development, and I have used technology stacks in my work, including Spring,SpringCloud, Springboot, Mybatis, Mysql, Paraphrase and other common technology stacks.https://hsbc.zoom.us/j/2081988808?pwd=emdlR2NMVkFvUUNzamdYZ1RXMUJvQT09
 
 My previous company was Digital China Technology Co., LTD. The project I was working on was the Global Documentation System.
 I was mainly responsible for separating microservices from the previous single architecture and making the system more maintainable and scalable.
