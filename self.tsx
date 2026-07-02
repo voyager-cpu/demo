cbb4fdf42fe1a8d7b157a3fd5b8e33e8f4cc8cbe
voyager-cpu <84560951+voyager-cpu@users.noreply.github.com>
2026-07-02 16:52:55 +0800

    Create self.tsx

---
+++ self.tsx
@@ -0,0 +1,11 @@
+Thank you for giving me the opportunity of this interview. My name is Qian Wenmeng. My English name is sorar. I come from Zhaotong City, Yunnan Province. I graduated from Chongqing University of Science and Technology.
+
+
+Since graduation, I have been engaged in Java development. The technology stacks I use in my work include Spring, SpringCloud, Springboot, Mybatis, Mysql, Redis, and other commonly used technology stacks.
+
+Since I started working, I have been engaged in Java development, and I have used technology stacks in my work, including Spring,SpringCloud, Springboot, Mybatis, Mysql, Paraphrase and other common technology stacks.
+
+My previous company was Digital China Technology Co., LTD. The project I was working on was the Global Documentation System.
+I was mainly responsible for separating microservices from the previous single architecture and making the system more maintainable and scalable.
+
+In my spare time, I play basketball and occasionally play games. Thank you. The above is my personal introduction and work experience.
